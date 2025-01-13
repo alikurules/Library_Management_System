21069e4fa5dd2dd1d75a14ccc1656d0dcc19c0eb
alikurules <alikurules@gmail.com>
2025-01-13 23:23:34 +0300

    Final Edit!

--- lmsproject/lmsapp/models.py
+++ lmsproject/lmsapp/models.py
@@ -48,11 +48,16 @@ class User(models.Model):
     def __str__(self):
         return self.username
 
+
 class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    checkout_date = models.DateField(auto_now_add=True)
-    return_date = models.DateField(null=True, blank=True)
+    checkout_date = models.DateTimeField(auto_now_add=True)
+    return_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} borrowed {self.book.title}'
+
 
     def clean(self):
         # Custom validation to ensure a book isn't checked out if no copies are available
